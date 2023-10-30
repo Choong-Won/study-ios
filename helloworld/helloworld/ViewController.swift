eefd74e59242903433393096dd93bec858c2a02b
choongwon <choongwon.ji@gmail.com>
2023-10-30 23:07:12 +0900

    [choong] connect click event of the button for changing text in the textview

--- helloworld/helloworld/ViewController.swift
+++ helloworld/helloworld/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // mutable
+    var mutableCounter: Int = 0
+    // immutable
+    let immutableCounter: Int = 0
 
+    
+    @IBOutlet weak var textLabel1: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnClick1(_ sender: Any) {
+        mutableCounter += 1
+        textLabel1.text = "click : \(mutableCounter)"
+    }
+    
 }
 
